52657fbd57a8afd0d4f852a4511b36dfb7f33312
Giovanni Mazzitelli <45018579+gmazzitelli@users.noreply.github.com>
2020-04-30 17:11:03 +0200

    Create colab_install_root.sh
    install CERN ROOT on google COLAB

---
+++ colab_install_root.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+mkdir -p APPS
+pwd
+cd APPS && wget -q https://root.cern.ch/download/root_v6.13.08.Linux-ubuntu18-x86_64-gcc7.3.tar.gz 
+cd APPS && tar -xf root_v6.13.08.Linux-ubuntu18-x86_64-gcc7.3.tar.gz
+pip install root-numpy
+apt-get install libdavix0v5
+echo "Davix.GSI.CACheck: n" >> $ROOTSYS/etc/system.rootrc
+cp $ROOTSYS/etc/system.rootrc $HOME/.rootrc
+tail -1 $HOME/.rootrc
+pip install -q  python-swiftclient
+pip install -q  keystoneauth1
+pip install -q  h5py
+wget -q https://raw.githubusercontent.com/gmazzitelli/cygno/master/cygnus_lib.py
+wget -q https://raw.githubusercontent.com/gmazzitelli/cygno/master/mylib.py
+mkdir -p data
+exit
