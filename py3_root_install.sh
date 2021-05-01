c6b42a38f96f3a18c241cf052c235b13a40a036e
Giovanni Mazzitelli <45018579+gmazzitelli@users.noreply.github.com>
2021-05-01 16:31:06 +0200

    Create py3_root_install.sh
    install ROOT on colab

---
+++ py3_root_install.sh
@@ -0,0 +1,10 @@
+wget https://github.com/MohamedElashri/HEP-ML/releases/download/ROOT/ROOT.tar.zip
+unzip /content/ROOT.tar.zip
+tar -xf  ROOT.tar
+apt-get install git dpkg-dev cmake g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev tar gfortran subversion
+install root_numpy # optional if you want to convert Trees to numpy arrays and other things like that
+wget https://raw.githubusercontent.com/gmazzitelli/cygno_cloud/main/cygnus_lib.py
+wget https://raw.githubusercontent.com/gmazzitelli/cygno/master/mylib.py
+madir out
+mkdir ped
+pip install root-numpy
