611da30ce52373e3cd8d9882baa169287ed4d96c
Giovanni Mazzitelli <45018579+gmazzitelli@users.noreply.github.com>
2021-05-01 16:49:11 +0200

    Update myenv.py

--- myenv.py
+++ myenv.py
@@ -1,10 +1,9 @@
-def root_env():
-  import sys
-  sys.path.append("/content/root_build/")
-  sys.path.append("/content/root_build/bin/")
-  sys.path.append("/content/root_build/include/")
-  sys.path.append("/content/root_build/lib/")
-  import ctypes
-  ctypes.cdll.LoadLibrary('/content/root_build/lib//libCore.so')
-  ctypes.cdll.LoadLibrary('/content/root_build/lib//libThread.so')
-  ctypes.cdll.LoadLibrary('/content/root_build/lib//libTreePlayer.so')
+import sys
+sys.path.append("/content/root_build/")
+sys.path.append("/content/root_build/bin/")
+sys.path.append("/content/root_build/include/")
+sys.path.append("/content/root_build/lib/")
+import ctypes
+ctypes.cdll.LoadLibrary('/content/root_build/lib//libCore.so')
+ctypes.cdll.LoadLibrary('/content/root_build/lib//libThread.so')
+ctypes.cdll.LoadLibrary('/content/root_build/lib//libTreePlayer.so')
